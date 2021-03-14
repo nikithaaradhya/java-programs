8bd223e75fc1d2fc3511510673ef25c010f33e37
nikitha.aradhya <nikitha.aradhya123@gmail.com>
2021-03-14 20:05:25 +0530

    Factorial of a number with user input

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num;
+        int fact=1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        num = input.nextInt();
+        for (int i=2;i<=num; i++){
+            fact=fact*i;
+        }
+
+        System.out.println("Factorial: "+fact);
+    }
+}
