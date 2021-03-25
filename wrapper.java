96f30b6ebe7dfc1df4b86dc1f15d98eee469760b
nikitha.aradhya <43704650+nikithaaradhya@users.noreply.github.com>
2021-03-25 11:12:13 +0530

    Simple Wrapper program

---
+++ wrapper.java
@@ -0,0 +1,22 @@
+package com.company;
+//Primitive Types to Wrapper Objects
+class Wrapper {
+    public static void main(String[] args) {
+
+        // create primitive types
+        int a = 5;
+        double b = 5.65;
+
+        //converts into wrapper objects
+        Integer aObj = Integer.valueOf(a);
+        Double bObj = Double.valueOf(b);
+
+        if(aObj instanceof Integer) {
+            System.out.println("An object of Integer is created.");
+        }
+
+        if(bObj instanceof Double) {
+            System.out.println("An object of Double is created.");
+        }
+    }
+}
